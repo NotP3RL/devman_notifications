2fd7c88238de52cd5e0081f818105747336631d8
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2023-08-17 17:56:03 +0800

    сделал код энергоэффективным

--- main.py
+++ main.py
@@ -39,3 +39,4 @@ if __name__ == '__main__':
                     bot.send_message(chat_id=chat_id, text=telegram_message)
         except (requests.exceptions.ConnectTimeout, ConnectionError):
             print('skill issue')
+            time.sleep(30)
